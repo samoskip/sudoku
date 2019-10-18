ad478b5e06c8bdd95ad785e8d461e45e8940d140
samskipper <samskipper@172.19.128.128>
2019-10-18 09:36:10 -0500

    Green Light |

--- sudoku/create.py
+++ sudoku/create.py
@@ -1,23 +1,21 @@
-from asyncio.windows_events import NULL
 def _create(parms):
     #result = {'status': 'create stub'}
     if (not ("level" in parms)):
         result = {'status': 'create stub'}
     else:
         result = parms["level"]      
-        levelNum = int(result)
         
-        if (levelNum == 1):
+        if (result == '1'):
             levelReturn = "This is level one"
-        elif (levelNum == 2):
+        elif (result == '2'):
             levelReturn = "This is level two"
-        elif (levelNum == 3 or result == NULL):
+        elif (result == '3' or result == ""):
             levelReturn = "This is level three"
-        elif (levelNum == 4):
+        elif (result == '4'):
             levelReturn = "This is level four"
-        elif (levelNum == 5):
+        elif (result == '5'):
             levelReturn = "This is level five"
-        elif (levelNum > 5) or (result < 1):
+        else:
             levelReturn = "{'status':'error: invalid level'}"
 
 
