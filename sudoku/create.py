455ff123fba6378deb706dadd9234ba8f23fe082
samskipper <samskipper@172.19.128.128>
2019-10-17 22:18:06 -0500

    Red Light |

--- sudoku/create.py
+++ sudoku/create.py
@@ -1,3 +1,8 @@
 def _create(parms):
-    result = {'status': 'create stub'}
+    #result = {'status': 'create stub'}
+    if (not ("level" in parms)):
+        result = {'status': 'create stub'}
+    else:
+        result = parms["level"]
+        
     return result
--- sudoku/test/createTest.py
+++ sudoku/test/createTest.py
@@ -2,4 +2,14 @@ from unittest import TestCase
 import sudoku.create as sudoku 
 
 class CreateTest(TestCase):
-        pass
+    
+    
+    def setUp(self):
+        self.testDict = {}
+        
+    def setUpDict(self, setLevel):
+        self.testDict["level"] = 1
+
+    def test100_shouldReturnLevel1Stub(self):
+        self.setUpDict(1)
+        self.assertEqual(sudoku._create(self.testDict), 1)
