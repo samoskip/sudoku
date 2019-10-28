8aa860cc6fb65a6a6404772fb06fab9f391659c9
samskipper <samskipper@192.168.1.5>
2019-10-28 16:19:36 -0500

    Green Light |

--- sudoku/test/insertTest.py
+++ sudoku/test/insertTest.py
@@ -17,7 +17,7 @@ class InsertTest(TestCase):
         self.testDict["integrity"] = setIntegrity
         
     def gridsToCall(self, gridNumber):
-        if (gridNumber == '1')
+        if (gridNumber == '1'):
             return [-8,-1,-5,-7,-6,-9,-3,-2,0,-4,-9,0,0,0,-5,-8,-7,0,0,0,-6,0,-4,-8,0,-9,-5,0,-8,-1,0,0,-3,0,0,-2,0,-5,0,-1,-8,0,-9,0,-7,-7,-3,-9,-5,-2,-4,-6,-8,-1,-9,-4,0,0,0,-7,0,-1,-8,-5,-2,0,-8,-9,0,-4,-6,-3,-1,-6,0,-4,-3,-2,-7,0,0]
         
     def test100_010_shouldInsertValueIntoNomialSpace(self):
