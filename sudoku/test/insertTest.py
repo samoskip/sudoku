f71d32ed6968f20df03cb003f2ad2b89741fb68e
samskipper <samskipper@192.168.1.5>
2019-10-28 17:34:46 -0500

    Yellow Light |

--- sudoku/test/insertTest.py
+++ sudoku/test/insertTest.py
@@ -8,6 +8,7 @@ class InsertTest(TestCase):
         self.error2 = {'status':'error: missing cell reference'}
         self.error3 = {'status':'error: attempt to change fixed hint'}
         self.error4 = {'status':'error: invalid grid'}
+        self.error5 = {'status':'error: integrity mismatch'}
         self.testDict = {}
         self.INVALIDLEVEL = {'status':'error: invalid level'}
         self.test1Grid = {'grid': [-8, -1, -5, -7, -6, -9, -3, -2, 0, -4, -9, 0, 0, 0, -5, -8, -7, 0, 3, 0, -6, 0, -4, -8, 0, -9, -5, 0, -8, -1, 0, 0, -3, 0, 0, -2, 0, -5, 0, -1, -8, 0, -9, 0, -7, -7, -3, -9, -5, -2, -4, -6, -8, -1, -9, -4, 0, 0, 0, -7, 0, -1, -8, -5, -2, 0, -8, -9, 0, -4, -6, -3, -1, -6, 0, -4, -3, -2, -7, 0, 0],
@@ -75,3 +76,8 @@ class InsertTest(TestCase):
         self.maxDiff = None
         self.setUpDict('r3c1', '3', self.gridsToCall('2'), '634dd6769e9b9a53ee4416edb9790684ac18dcbde5b879260610ff27794b66f5')
         self.assertEqual(sudoku._insert(self.testDict), self.error4)
+        
+    def test900_040_shouldReturnIntegrityMismatchError(self):
+        self.maxDiff = None
+        self.setUpDict('r3c1', '3', self.gridsToCall('1'), '0000000000000000000000000000000000000000000000000000000000000000000')
+        self.assertEqual(sudoku._insert(self.testDict), self.error5)
